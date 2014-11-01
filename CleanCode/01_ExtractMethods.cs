e767682bcd67a603fca977c0442329981e48aebd
anotherbugmaster <anotherbugmaster@gmail.com>
2014-11-01 18:03:28 +0500

    - Lalala

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -8,23 +8,19 @@ namespace CleanCode
 		private static void SaveData(string fileName, byte[] data)
 		{
 			//open files
-			var fileStream = new FileStream(fileName, FileMode.OpenOrCreate);
-			var backupStream = new FileStream(Path.ChangeExtension(fileName, "bkp"), FileMode.OpenOrCreate);
+            WriteData(fileName, data);
+            WriteData(Path.ChangeExtension(fileName, "bkp"), data);
 
-			// write data
-			fileStream.Write(data, 0, data.Length);
-			backupStream.Write(data, 0, data.Length);
-
-			// close files
-			fileStream.Close();
-			backupStream.Close();
-
-			// save last-write time
-			string tf = fileName + ".time";
-			var dateFileStream = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			dateFileStream.Write(t, 0, t.Length);
-			dateFileStream.Close();
+            string timeFileName = fileName + ".time";
+            var ticks = BitConverter.GetBytes(DateTime.Now.Ticks);
+            WriteData(timeFileName, ticks);
 		}
+
+	    private static void WriteData(string fileName, byte[] data)
+	    {
+	        var fileStream = new FileStream(fileName, FileMode.OpenOrCreate);
+	        fileStream.Write(data, 0, data.Length);
+	        fileStream.Close();
+	    }
 	}
 }
